be7dc833e3ff2e730c32773f4a5b481d96d81845
hamzasisman <hamza.sismann@gmail.com>
2022-11-01 09:16:48 +0300

    First Day was completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import styles from './App.module.css';
+import React, { Component }  from 'react';
+import { Title } from "./Component.js";
 
 function App() {
+  useEffect(() => {
+    console.log(styles);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <Title>{process.env.NODE_ENV}</Title>
+        <h3>{process.env.REACT_APP_API_URL}</h3>
+
+
+        {/* Sadece build alındığında yani development ortamında görünür (diğer ortan production) */}
+        {process.env.NODE_ENV === 'development' && (
+          <>
+            <img src={logo} className={styles.AppLogo} alt="logo" />
+          </>
+        )}
+        {/* *********** */}
+
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
