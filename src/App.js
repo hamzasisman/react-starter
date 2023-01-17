f79905ab9a84444c23b0118a16e12b3d57d35f3d
hamzasisman <hamza.sismann@gmail.com>
2023-01-17 17:57:35 +0300

    tailwind is working with react

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import logo from './logo.svg';
-import { useEffect } from "react";
-import styles from './App.module.css';
-import React, { Component }  from 'react';
-import { Title } from "./Component.js";
+import './App.css';
 
 function App() {
-  useEffect(() => {
-    console.log(styles);
-  }, [])
-
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <Title>{process.env.NODE_ENV}</Title>
-        <h3>{process.env.REACT_APP_API_URL}</h3>
-
-
-        {/* Sadece build alındığında yani development ortamında görünür (diğer ortan production) */}
-        {process.env.NODE_ENV === 'development' && (
-          <>
-            <img src={logo} className={styles.AppLogo} alt="logo" />
-          </>
-        )}
-        {/* *********** */}
-
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline text-red-500 text-center">
+      Hello world!
+    </h1>
   );
 }
 
--- src/Component.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Title = styled.h1`
-    font-size: 36px;
-    font-weight: bold;
-    text-decoration: underline;
-`
